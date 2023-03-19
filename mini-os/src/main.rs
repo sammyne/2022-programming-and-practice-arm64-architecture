6616a4152d05e8d43ad11928566d0956502b6081
sammyne <xiangminli@alumni.sjtu.edu.cn>
2023-03-19 07:52:58 +0000

    feat(chapter03): suspend lab06

--- mini-os/src/main.rs
+++ mini-os/src/main.rs
@@ -13,6 +13,7 @@ global_asm!(include_str!("lab02.s"));
 global_asm!(include_str!("lab03.s"));
 global_asm!(include_str!("lab04.s"));
 global_asm!(include_str!("lab05.s"));
+global_asm!(include_str!("lab06.s"));
 
 global_asm!(include_str!("breakpoint_hook.s"));
 
@@ -22,6 +23,7 @@ extern "C" {
     fn lab03();
     fn lab04();
     fn lab05();
+    fn lab06_memset_aligned_16bytes(s: *mut u8, data: u64, count: u64);
 
     fn breakpoint_hook();
 }
@@ -48,8 +50,51 @@ pub extern "C" fn not_main() {
 
         lab04();
         breakpoint_hook(); // 添加钩子函数辅助断点调试
-        
+
         lab05();
         breakpoint_hook(); // 添加钩子函数辅助断点调试
+
+        // FIXME: this is buggy for unknown reasons
+        //let mut buf = [0u8; 32];
+        //let _ = memset_impl(&mut buf, 123);
     }
+}
+
+unsafe fn memset_aligned_1byte(buf: *mut u8, v: u8, n: u64) {
+    let mut b = buf;
+    for _i in 0..n {
+        *b = v;
+        b = b.offset(1);
+    }
+}
+
+unsafe fn memset_impl(buf: &mut [u8], v: u8) -> &mut [u8] {
+    const ALIGN: u64 = 16;
+
+    let mut p: *mut u8 = buf.as_mut_ptr();
+    let addr = p as u64;
+    let mut remaining = buf.len() as u64;
+
+    if (addr & (ALIGN - 1)) != 0 {
+        let unaligned_len = ALIGN - (addr & (ALIGN - 1));
+        memset_aligned_1byte(p, v, unaligned_len);
+        p = p.offset(unaligned_len as isize);
+        remaining -= unaligned_len;
+    }
+
+    let data = {
+        let mut out: u64 = 0;
+        let vv = v as u64;
+
+        for _i in 0..8 {
+            out = (out << 8) | vv;
+        }
+        out
+    };
+
+    if remaining >= ALIGN {
+        lab06_memset_aligned_16bytes(p, data, remaining / ALIGN);
+    }
+
+    buf
 }
